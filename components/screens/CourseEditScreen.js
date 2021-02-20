e0c9b56c5ae8c50f569b7e0b34ece263d6afcce7
ptarasul18 <paulina.tarasul@gmail.com>
2021-02-19 19:40:31 -0600

    store data

--- components/screens/CourseEditScreen.js
+++ components/screens/CourseEditScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Form from '../expo-form-starter/Form.js';
 import * as Yup from 'yup';
+import { firebase } from '../../utils/firebase.js';
+import { useContext, useState, useEffect } from 'react';
 
 const Field = ({label, value}) => {
   return (
@@ -27,6 +29,15 @@ const validationSchema = Yup.object().shape({
 
 const CourseEditScreen = ({navigation, route}) => {
   const course = route.params.course;
+  const [submitError, setSubmitError] = useState('');
+
+  async function handleSubmit(values) {
+    const { id, meets, title } = values;
+    const course = { id, meets, title };
+    firebase.database().ref('courses').child(id).set(course).catch(error => {
+      setSubmitError(error.message);
+    });
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -36,8 +47,9 @@ const CourseEditScreen = ({navigation, route}) => {
         id: course.id,
         meets: course.meets,
         title: course.title,
-        validationSchema: {validationSchema},
-        }}>
+        }}
+        validationSchema= {validationSchema}
+        onSubmit= {values => handleSubmit(values)}>
           <Form.Field
             name="id"
             leftIcon="identifier"
@@ -56,6 +68,8 @@ const CourseEditScreen = ({navigation, route}) => {
             leftIcon="format-title"
             placeholder="Introduction to programming"
           />
+          <Form.Button title={'Update'} />
+          {<Form.ErrorMessage error={submitError} visible={true} />}
         </Form>
       </ScrollView>
     </SafeAreaView>
--- components/screens/ScheduleScreen.js
+++ components/screens/ScheduleScreen.js
@@ -6,22 +6,22 @@ import { useContext, useState, useEffect } from 'react';
 import CourseSelector from '../CourseSelector';
 import UserContext from '../UserContext';
 import CourseEditScreen from './CourseEditScreen';
-//import firebase from '../../firebase.js';
-import firebase from "firebase/app";
-import "firebase/database";
+import { firebase } from '../../utils/firebase.js';
+// import firebase from "firebase/app";
+// import "firebase/database";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDpFbn3YHC5vLLiv6bbVkW__SUUqwEnGrI",
-  authDomain: "coursescheduler-6113c.firebaseapp.com",
-  databaseURL: "https://coursescheduler-6113c-default-rtdb.firebaseio.com",
-  projectId: "coursescheduler-6113c",
-  storageBucket: "coursescheduler-6113c.appspot.com",
-  messagingSenderId: "888186878247",
-  appId: "1:888186878247:web:6e9a8bd499691b52a5d102",
-  measurementId: "G-76GDDNXFBP"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDpFbn3YHC5vLLiv6bbVkW__SUUqwEnGrI",
+//   authDomain: "coursescheduler-6113c.firebaseapp.com",
+//   databaseURL: "https://coursescheduler-6113c-default-rtdb.firebaseio.com",
+//   projectId: "coursescheduler-6113c",
+//   storageBucket: "coursescheduler-6113c.appspot.com",
+//   messagingSenderId: "888186878247",
+//   appId: "1:888186878247:web:6e9a8bd499691b52a5d102",
+//   measurementId: "G-76GDDNXFBP"
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
 const schedule = {
   title: "CS Courses for 2018-2019",
